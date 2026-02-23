966fb03e8d38865abd68e12d820cf20fe8a77a51
vyeos <rudranpatel0980@gmail.com>
2026-02-23 16:45:21 +0530

    setup: better-auth,drizzle,shadcn

---
+++ lib/auth.ts
@@ -0,0 +1,20 @@
+import { db } from "@/db";
+import { betterAuth } from "better-auth";
+import { drizzleAdapter } from "better-auth/adapters/drizzle";
+import * as schema from "@/db/schema";
+
+export const auth = betterAuth({
+  database: drizzleAdapter(db, {
+    provider: "pg",
+    schema: schema,
+  }),
+  emailAndPassword: {
+    enabled: true,
+  },
+  socialProviders: {
+    github: {
+      clientId: process.env.GITHUB_CLIENT_ID as string,
+      clientSecret: process.env.GITHUB_CLIENT_SECRET as string,
+    },
+  },
+});
